9e46b7ae20ff1d89746ea59cdc31fb44f93d3905
Kaushik Karthikeyan <66272646+kaushikcodes@users.noreply.github.com>
2023-06-08 10:08:01 -0400

    Update Controller.cpp

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -5,24 +5,33 @@
 const int pin1 = 18;
 const int pin2 = 10;
 const int pwma = 33;
-
-const int extendtime = 3000;
-uint16_t retracttime = 1700;
+//bool resetIsPressed = false;
 
 uint16_t count = 0;
-uint16_t threshold = 2300;
+uint16_t threshold = 2300; //CHANGE THIS VALUE TO CHANGE DEFAULT NUMBER OF CYCLES
 
 Controller_t instance;
+uint8_t currSpeed;
+uint16_t speedDelays[] = {1550, 2000, 2500, 3400, 4000}; //Change these values for each speed and see which one fits best
+enum {
+    speed_2,
+    speed_4,
+    speed_6,
+    speed_8,
+    speed_10
+};
 
+// ALWAYS KEEP POWER PLUGGED IN AT THE START
 
 void Controller_Init(tiny_timer_group_t *timerGroup)
 {
     instance._private.timerGroup = timerGroup;
     pinMode(pin1, OUTPUT);
     pinMode(pin2, OUTPUT);
-
-    digitalWrite(pin1, HIGH);
+//EXTENDS ALL THE WAY IN THE START
+    digitalWrite(pin1, LOW);
     digitalWrite(pin2, HIGH);
+    delay(3000);
 
     Serial.print("Initializing");
     Controller_Extend(NULL);
@@ -38,7 +47,7 @@ void Controller_Extend(void *context)
     tiny_timer_start(
         instance._private.timerGroup,
         &instance._private.controllerTimer,
-        extendtime,
+        speedDelays[currSpeed],
         NULL,
         Controller_Hold
     );
@@ -51,12 +60,12 @@ void Controller_Hold(void *context)
     digitalWrite(pin1, HIGH); //MOTOR A ON THE LEFT
     digitalWrite(pin2, HIGH); //MOTOR A ON THE LEFT
 
-    int delaytime = ((((retracttime - 1000) / 350) - 1) * 500);
+    //int delaytime = ((((retracttime - 1000) / 350) - 1) * 500);
     Serial.print("Holding");
     tiny_timer_start(
         instance._private.timerGroup,
         &instance._private.controllerTimer,
-        delaytime,
+        1500,
         NULL,
         Controller_Retract
     );
@@ -65,27 +74,33 @@ void Controller_Hold(void *context)
 void Controller_Retract(void *context)
 {
     Serial.print("Retracting");
-    if(count > threshold)
-    {
-        delay(10000000);
-    }
     count += 1;
-    digitalWrite(pin1, HIGH); //MOTOR A ON THE LEFT
-    digitalWrite(pin2, LOW); //MOTOR A ON THE LEFT
-    tiny_timer_start(
-        instance._private.timerGroup,
-        &instance._private.controllerTimer,
-        retracttime,
-        NULL,
-        Controller_HoldAtEnd
-    );
+
+    if(count < threshold) //COMMENT OUT THIS IF-STATEMENT IF RUNNING INFINITELY
+    {
+        digitalWrite(pin1, HIGH);
+        digitalWrite(pin2, LOW);
+        tiny_timer_start(
+            instance._private.timerGroup,
+            &instance._private.controllerTimer,
+            speedDelays[currSpeed],
+            NULL,
+            Controller_HoldAtEnd
+        );
+    }
+}
+
+void Controller_Reset()
+{
+    count = 0;
+    Controller_Init(NULL); // NEED TO CHANGE
 }
 
 void Controller_HoldAtEnd(void *context)
 {
 
-    digitalWrite(pin1, HIGH); //MOTOR A ON THE LEFT
-    digitalWrite(pin2, HIGH); //MOTOR A ON THE LEFT
+    digitalWrite(pin1, HIGH);
+    digitalWrite(pin2, HIGH);
     Serial.print("Holding");
     tiny_timer_start(
         instance._private.timerGroup,
@@ -108,34 +123,40 @@ void Controller_DecreaseThreshold()
 
 void Controller_IncreaseSpeed()
 {
-    if(retracttime < 2800)
+    if(currSpeed < 5)
     {
-        retracttime = retracttime + 350;
+        currSpeed += 1;
     }
     else
     {
-        retracttime = retracttime;
+        currSpeed = currSpeed;
     }
 }
 
 void Controller_DecreaseSpeed()
 {
-    if(retracttime > 1351)
+    if(currSpeed > 0)
     {
-        retracttime = retracttime - 350;
+        currSpeed -= 1;
     }
     else
     {
-        retracttime = retracttime;
+        currSpeed = currSpeed;
     }
 }
 
 int Controller_GetTime()
 {
-    return retracttime;
+    return speedDelays[currSpeed];
+}
+
+int Controller_GetSetting()
+{
+    return (currSpeed + 1) * 2;
 }
 
 int Controller_GetThreshold()
 {
     return threshold;
 }
+
