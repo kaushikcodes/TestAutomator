493c18fc9a4f1a5c2e46e1382d66b6be50ffb9a5
Kaushik Karthikeyan <66272646+kaushikcodes@users.noreply.github.com>
2023-06-12 08:27:08 -0400

    Update Display.cpp

--- src/Display.cpp
+++ src/Display.cpp
@@ -7,7 +7,8 @@
 ThinkInk_290_Grayscale4_T5 display(EPD_DC, EPD_RESET, EPD_CS, -1);
 int speedy = Controller_GetTime();
 int setting = Controller_GetSetting();
-int thresholdy = Controller_GetThreshold();
+int counter = Controller_GetCount();
+int timeAtSpeed = Controller_GetHoldAtSpeedTime();
 ThinkInk_290_Grayscale4_T5* Display_GetPtr(void)
 {
    return &display;
@@ -34,18 +35,22 @@ void Display_FullRefreshFromBuffer(void)
 
 void Display_Init(void)
 {
+   if(setting == 6)
+   {
+      setting = 10;
+   }
    display.begin(THINKINK_MONO);
    display.clearBuffer();
    display.display();
    display.setTextColor(EPD_BLACK, EPD_LIGHT);
    display.setTextSize(3);
    display.printf("KitchenAid Mixer");
-   display.setCursor(25, 120);
+   display.setCursor(10, 120);
    display.setTextSize(1.8);
-   display.printf("- Cycles +");
+   display.printf("Count");
    display.setCursor(30, 90);
    display.setTextSize(2);
-   display.print(thresholdy);
+   display.print(counter);
    display.setCursor(210, 90);
    display.setTextSize(2);
    display.print(setting);
@@ -59,18 +64,24 @@ void Display_Init(void)
 void Display_Run(void)
 {
     int speedy2 = Controller_GetTime();
-    int thresholdy2 = Controller_GetThreshold();
+    int count2 = Controller_GetCount();
+    //int thresholdy2 = Controller_GetThreshold();
+    int timeAtSpeedNew = Controller_GetHoldAtSpeedTime();
     if(speedy2 != speedy)
     {
         speedy = speedy2;
         setting = Controller_GetSetting();
+         if(setting == 6)
+         {
+            setting = 10;
+         }
         display.setCursor(205, 90);
         display.setTextSize(2);
         display.print(setting);
         display.print(" ");
         display.display();
     }
-
+/*
     if(thresholdy2 != thresholdy)
     {
         thresholdy = thresholdy2;
@@ -80,4 +91,29 @@ void Display_Run(void)
         display.print(" ");
         display.display();
     }
+*/
+
+/*
+   if(timeAtSpeedNew != timeAtSpeed)
+    {
+        timeAtSpeed = timeAtSpeedNew;
+        display.setCursor(30, 90);
+        display.setTextSize(2);
+        display.print(timeAtSpeed);
+        display.print(" ");
+        display.display();
+    }
+*/
+   if(count2 != counter)
+   {
+      counter = count2;
+      display.setCursor(30, 90);
+      display.setTextSize(2);
+      display.print(counter);
+      display.print(" ");
+      display.display();
+   }
+
+
+
 }
