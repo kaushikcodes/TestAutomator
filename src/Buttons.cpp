aa5a62891eec29eb59211f12a491ca4e0b73209c
Kaushik Karthikeyan <66272646+kaushikcodes@users.noreply.github.com>
2023-06-30 16:21:43 -0400

    Update Buttons.cpp

--- src/Buttons.cpp
+++ src/Buttons.cpp
@@ -4,65 +4,56 @@
 #include "Controller.h"
 #include <Arduino.h>
 
-Button2 btnA(PIN_BTN_A, INPUT_PULLUP);
-Button2 btnB(PIN_BTN_B, INPUT_PULLUP);
-Button2 btnC(PIN_BTN_C, INPUT_PULLUP);
-Button2 btnD(PIN_BTN_D, INPUT_PULLUP);
+Button2 btnA(PIN_BTN_A, INPUT_PULLDOWN, false);
+Button2 btnB(PIN_BTN_B, INPUT_PULLDOWN, false);
+Button2 btnC(PIN_BTN_C, INPUT_PULLUP, true);
+bool PowerOn = true;
 
 void Buttons_Run(void)
 {
    btnA.loop();
    btnB.loop();
    btnC.loop();
-   btnD.loop();
 }
+
 
 void ButtonAClicked(Button2 &button)
 {
     Serial.print("Button A was clicked!     ");
-    //Controller_DecreaseThreshold();
-    Controller_DecreaseHoldTimeAtSpeed();
+    //Controller_DecreaseSpeed();
 }
 
 void ButtonBClicked(Button2 &button)
 {
-    Serial.print("Button B was clicked!      ");
-    //Controller_IncreaseThreshold();
-    Controller_IncreaseHoldTimeAtSpeed();
+    Serial.print("Button B was clicked!     ");
+    Controller_IncreaseSpeed();
 }
 
 void ButtonCClicked(Button2 &button)
 {
     Serial.print("Button C was clicked!     ");
-    Controller_DecreaseSpeed();
+    //Controller_DecreaseSpeed();
+    PowerOn = !PowerOn;
+    Controller_EmergencyStop(); //EXTEND ALL THE WAY BY EXACT AMOUNT
 }
 
-void ButtonDClicked(Button2 &button)
-{
-    Serial.print("Button D was clicked!     ");
-    Controller_IncreaseSpeed();
-}
-
-void ResetMode(Button2 &button)
-{
-    Serial.print("Button D was long pressed!     ");
-    Controller_Reset();
-}
 
 void Buttons_Init(void)
 {
 
-   btnA.begin(PIN_BTN_A, INPUT_PULLUP);
-   btnB.begin(PIN_BTN_B, INPUT_PULLUP);
-   btnC.begin(PIN_BTN_C, INPUT_PULLUP);
-   btnD.begin(PIN_BTN_D, INPUT_PULLUP);
+   btnA.begin(PIN_BTN_A, INPUT_PULLDOWN, false);
+   btnB.begin(PIN_BTN_B, INPUT_PULLDOWN, false);
+   btnC.begin(PIN_BTN_C, INPUT_PULLUP, true);
 
    btnA.setClickHandler(ButtonAClicked);
    btnB.setClickHandler(ButtonBClicked);
    btnC.setClickHandler(ButtonCClicked);
-   btnD.setClickHandler(ButtonDClicked);
-   btnD.setLongClickTime(1500);
-   btnD.setLongClickHandler(ResetMode);
+   btnC.setLongClickTime(3000);
+   btnC.setLongClickHandler(ResetMode);
 }
-//LONG PRESS BUTTON D FOR 1.5 SECONDS TO RESET COUNTER FOR NO OF CYCLES
 
+bool Buttons_GetPower()
+{
+    return PowerOn;
+}
+//LONG PRESS BUTTON c FOR 1.5 SECONDS TO RESET COUNTER FOR NO OF CYCLES
