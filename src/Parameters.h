e86e2d643d4e4c0059ef18497f60ee6b10a75cf6
Kaushik Karthikeyan <66272646+kaushikcodes@users.noreply.github.com>
2023-06-30 16:21:25 -0400

    Update Parameters.h

--- src/Parameters.h
+++ src/Parameters.h
@@ -1,5 +1,12 @@
-#define PIN_BTN_A       15
-#define PIN_BTN_B       14
-#define PIN_BTN_C       12
-#define PIN_BTN_D       11
+#define PIN_BTN_A       1
+#define PIN_BTN_B       2
+#define PIN_BTN_C       0
+
+/*
+#define TFT_CS_1          7
+#define TFT_DC_2          39
+#define TFT_RESET         40
+#define TFT_BACKLIGHT     45
+
+*/
 #define PARAMETERS_BUTTON_UPDATE_PERIOD_MS         (10)
