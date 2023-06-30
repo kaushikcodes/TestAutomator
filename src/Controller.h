4ab102281b1ca50908c28b8973ec4e7dbf6a12ad
Kaushik Karthikeyan <66272646+kaushikcodes@users.noreply.github.com>
2023-06-30 16:19:21 -0400

    Update Controller.h

--- src/Controller.h
+++ src/Controller.h
@@ -16,19 +16,20 @@ typedef struct {
 
 void Controller_Init(tiny_timer_group_t *timerGroup);
 void Controller_Retract(void *context);
-void Controller_Hold(void *context);
 void Controller_HoldAtEnd(void *context);
-void Controller_Extend(void *context);
 void Controller_IncreaseSpeed();
-void Controller_DecreaseSpeed();
-void Controller_IncreaseThreshold();
-void Controller_DecreaseThreshold();
 int Controller_GetCount();
 int Controller_GetTime();
 int Controller_GetSetting();
-void Controller_Reset();
-int Controller_GetHoldAtSpeedTime();
-void Controller_IncreaseHoldTimeAtSpeed();
-void Controller_DecreaseHoldTimeAtSpeed();
+void Controller_PushToZero();
+bool Controller_CanHear();
+void Controller_EmergencyStop();
+int Controller_GetThreshold();
+void Controller_Run(void *context);
+void StopActuatorAtZero();
+bool ButtonIsPressed();
+void ExtendActuator();
+void RetractActuator();
+void StopActuatorAtSpeed();
 
 
