8609fa11ccf1f835c31544fe726501582c5cc889
Kaushik Karthikeyan <66272646+kaushikcodes@users.noreply.github.com>
2023-06-30 16:21:11 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,23 @@ static i_tiny_time_source_t *timeSource = tiny_time_source_init();
 
 void setup()
 {
-  Serial.begin();
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Starting Setup");
+  Serial.flush();
+  delay(1000);
   tiny_timer_group_init(&timerGroup, timeSource);
   Display_Init();
   Buttons_Init();
   Controller_Init(&timerGroup);
+  Serial.println("Finished Setup");
+  delay(1000);
 }
 
 void loop()
 {
   Buttons_Run();
+  Controller_Run(NULL);
   tiny_timer_group_run(&timerGroup);
   Display_Run();
 }
